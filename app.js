8fdddd438dc2ee3b2b73b8ada721c2a0ce110091
Oshan12345 <bsagor99@gmail.com>
2021-05-25 17:44:28 +0600

    basic of type script

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-var num = 9;
-num = 8;
+console.log("hello");
+var input1 = document.getElementById("inputOne");
+var input2 = document.getElementById("inputTwo");
+var addButton = document.getElementById("add-button");
+function add(a, b, showResult, phrase) {
+    if (showResult)
+        return phrase + " " + (a + b);
+}
+addButton.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value, true, "Your result is-"));
+});
+var number1 = 5;
+var number2 = 6;
+var number3;
+var total = add(number1, number2, true, "Your result is-");
+console.log(total);
+var student = {
+    name: "oshan",
+    id: 1,
+    "class": 10,
+    hobbies: ['sports', 'travelling']
+};
+console.log(student.name);
+for (var _i = 0, _a = student.hobbies; _i < _a.length; _i++) {
+    var element = _a[_i];
+    console.log(element.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,12 +1,44 @@
-let num:number
+console.log("hello");
 
-num=7
+const input1 = document.getElementById("inputOne")! as HTMLInputElement;
+const input2 = document.getElementById("inputTwo")! as HTMLInputElement;
+
+const addButton = document.getElementById("add-button") as HTMLButtonElement;
+function add(a:number, b:number,showResult:boolean,phrase:string) {
+
+if(showResult) return `${phrase} ${a+b}`
+}
+
+addButton.addEventListener("click", function() {
+  console.log(add(+input1.value , + input2.value,true,"Your result is-"));
+});
+
+let number1=5
+let number2=6
+let number3:number;
 
 
-console.log(num)
+const total=add(number1,number2,true,"Your result is-")
+
+console.log(total)
 
 
-const clintName:string='dfsdfsd'
 
-let isCorrect:boolean=true
+const student:{
+name:string;
+id:number;
+class:number;
+hobbies:string[]
+}={
+name:"oshan",
+id:1,
+class:10,
+hobbies:['sports','travelling']
+}
 
+
+console.log(student.name)
+
+for (const element of student.hobbies) {
+  console.log(element.toUpperCase());
+}
